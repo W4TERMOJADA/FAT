456e070756481750a205cb8f0cb7662f0b069e9d
w4ter <w4ter@localhost>
2025-05-12 10:34:08 +0200

    v1.2

---
+++ Fat/src/MetaCluster.java
@@ -0,0 +1,15 @@
+
+public class MetaCluster {
+	
+	int id;
+	
+	Boolean A;
+	Boolean D;
+	Boolean R;
+	int S;
+	Boolean E;
+	
+	public MetaCluster(int id) {
+		this.id=id;
+	}
+}
--- Fat/src/dataCluster.java
+++ Fat/src/dataCluster.java
@@ -1,5 +1,15 @@
 
 public class dataCluster {
-	private Metacluster metaCluster;
+	private MetaCluster metaCluster;
+	private GenericFile genericFile;
+	private static int nextId = 1; 
+    private final int id;
+    
+    public dataCluster() {
+    	this.id=nextId++;
+    	this.metaCluster=new MetaCluster(id);
+    }
+    
+    public updateMetaCluster
 	
 }
